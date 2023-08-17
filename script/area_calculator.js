8e686ae4d057b3db011b8dfa2bd9a99a81e8e9d1
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-08-18 01:19:08 +0600

    added calculation for rectangle card

--- script/area_calculator.js
+++ script/area_calculator.js
@@ -6,5 +6,31 @@ function btnTriangle() {
     //height field value
     const heightValueString = document.getElementById('triangle-height').value;
     const heightValue = parseFloat(heightValueString);
-    console.log(baseValue)
+    document.getElementById('triangle-base').value = '';
+    document.getElementById('triangle-height').value = '';
+    if(isNaN(baseValue) || isNaN(heightValue)){
+        alert('provide valid numbers');
+        return;
+    }
+    //calculate the area...
+    const triangleArea = 0.5 * baseValue * heightValue;
+    document.getElementById('triangle-area').innerText = triangleArea;
+}
+function btnRectangle(){
+    //width field value
+    const widthValueString = document.getElementById('rectangle-width').value;
+    const widthValue = parseFloat(widthValueString);
+
+    //lenght field value..
+    const lenghtValueString = document.getElementById('rectangle-length').value;
+    const lenghtValue = parseFloat(lenghtValueString);
+    document.getElementById('rectangle-width').value = '';
+    document.getElementById('rectangle-length').value = '';
+    if(isNaN(widthValue) || isNaN(lenghtValue)){
+        alert('provide valid numbers');
+        return;
+    }
+    //calculate the area..
+    const rectangleArea = widthValue * lenghtValue;
+    document.getElementById('rectangle-area').innerText = rectangleArea;
 }
