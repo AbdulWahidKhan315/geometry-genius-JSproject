fd124d60867a6d46f0e01bc1e32d2d89dc517541
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-08-17 23:58:20 +0600

    created the first three cards and added calculation for triangle card

---
+++ script/area_calculator.js
@@ -0,0 +1,10 @@
+function btnTriangle() {
+    //base field value
+    const baseValueString = document.getElementById('triangle-base').value;
+    const baseValue = parseFloat(baseValueString);
+    
+    //height field value
+    const heightValueString = document.getElementById('triangle-height').value;
+    const heightValue = parseFloat(heightValueString);
+    console.log(baseValue)
+}
